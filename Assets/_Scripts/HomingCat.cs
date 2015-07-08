5352b83b830651b36b3542760a3f03243fdb161c
Edward Huang <yukinishizawa@hotmail.co.jp>
2015-07-07 21:45:36 -0700

    Gave a rough Homing AI for the Homing Cat, added a new script and built a NavMesh to PlayWithEdward.unity ONLY

---
+++ Assets/_Scripts/HomingCat.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HomingCat : MonoBehaviour {
+
+	public Transform player;
+	NavMeshAgent navAgent;
+
+	// Use this for initialization
+	void Start () {
+		navAgent = GetComponent<NavMeshAgent> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		navAgent.SetDestination (player.position);
+	}
+}
