7e98191fc50f1d6deb66ccc2c3922e885a912326
Nbalsh <brandon.bn.nelson@gmail.com>
2015-07-09 23:55:06 -0700

    Added items bottom right corner
    can't use them though, too lazy to add one line of code for now

---
+++ Assets/_Scripts/GameManagerBN.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameManagerBN : MonoBehaviour {
+
+	public Collectable item;
+	public Sprite blankSprite;
+
+	private Canvas canvas;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void setItem(Collectable colItem){
+		item = colItem;
+	}
+
+	public void useItem(){
+		if (item != null) {
+			useItemAndSwitch();
+			GameObject.Destroy(item.gameObject);
+		}
+	}
+
+	public void useItemAndSwitch(){
+		Image[] images = canvas.GetComponentsInChildren<Image> ();
+		Image imgToChange = images [0];
+		Image imgToChange2 = images [1];
+		imgToChange.sprite = blankSprite;
+		imgToChange2.sprite = blankSprite;
+	}
+}
---
+++ Assets/_Scripts/PickUpItemBN.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PickUpItemBN : MonoBehaviour {
+	private GameObject gameManager;
+
+	public Canvas canvas;
+	public Image[] images;
+	public Sprite sprite;
+	private Collectable collectable;
+	private bool hasItem = false;
+
+	// Use this for initialization
+	void Start () {
+		gameManager = GameObject.FindWithTag ("GameManager");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//NEED CARDBOARDHEAD / Magnetic trigger
+		if (collectable != null && !hasItem) {
+			PickUpItem();
+		}
+	}
+
+	void PickUpItem(){
+		gameManager.GetComponent<GameManagerBN> ().setItem (collectable);
+		hasItem = true;
+		images = canvas.GetComponentsInChildren<Image> ();
+		Image imageToPut = images [0];
+		Image imageToPut2 = images [1];
+		imageToPut.sprite = collectable.sprite;
+		imageToPut2.sprite = collectable.sprite;
+		Renderer rend = collectable.GetComponent<Renderer> ();
+		Collider col = collectable.GetComponent<Collider> ();
+		rend.enabled = col.enabled = false;
+		collectable = null;
+	}
+
+	void UseItem(){
+		gameObject.GetComponent<GameManagerBN> ().useItem();
+		hasItem = false;
+	}
+
+	void OnTriggerEnter(Collider other){
+		var coll = other.gameObject.GetComponent<Collectable> ();
+		if (coll != null) {
+			collectable = coll;
+		} else {
+			collectable = null;
+		}
+	}
+
+	void onTriggerExit(Collider other){
+		var coll = other.gameObject.GetComponent<Collectable> ();
+		if (coll == null) {
+			collectable = null;
+		} else {
+			// is this okay?
+			collectable = null;
+		}
+	}
+}
